f401d5ca19a1928f365e4e0f77b888d05f88e7cd
MinJuTur <jmj4701jmj@gmail.com>
2025-01-07 08:42:52 +0900

    renamed

--- src/Bronze_3/Day2/BOJ2884_1.java
+++ src/Bronze_3/Day2/BOJ2884_1.java
@@ -2,20 +2,21 @@ package Bronze_3.Day2;
 
 import java.util.Scanner;
 
-public class BOJ2884 {
+public class BOJ2884_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int H = sc.nextInt();
         int M = sc.nextInt();
 
-        if (M <= 45) {
-            M += 15;
+        //M-45 가 H를 바꾸는지 확인
+        if (M - 45 < 0) {
             if (H == 0) {
                 H = 23;
             } else {
                 H--;
             }
+            M += 15;
         } else {
             M -= 45;
         }
