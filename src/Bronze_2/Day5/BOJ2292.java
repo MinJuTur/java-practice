ea4e09a4539568bef3038e47b840ddab70265039
MinJuTur <jmj4701jmj@gmail.com>
2025-01-07 16:34:59 +0900

    Bronze_2 Day5-undone

---
+++ src/Bronze_2/Day5/BOJ2292.java
@@ -0,0 +1,21 @@
+package Bronze_2.Day5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ2292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double N = ((double) sc.nextInt() + 4.0) / 6.0;
+        int roomCount;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        int temp = 0;
+        for (int i = 1; i <= ; i++) {
+            temp += i;
+            arr.add(temp);
+        }
+
+        System.out.println(roomCount);
+    }
+}
