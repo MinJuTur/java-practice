b08d60eff1940ebfb967533da912e11a0f0128b3
MinJuTur <jmj4701jmj@gmail.com>
2025-01-07 15:11:01 +0900

    Bronze_2 Day4-re

--- src/Bronze_2/Day4/BOJ2750_2.java
+++ src/Bronze_2/Day4/BOJ2750_2.java
@@ -19,9 +19,9 @@ public class BOJ2750_2 {
         for (int i = 1; i < N; i++) { //N=5, i=1,2,3,4
             for (int j = 0; j < N - i; j++) { //j=0,1,2,3  j=0,1,2  j=0,1  j=0 (총 (N-1)번 회전)
                 if (nums[j] > nums[j + 1]) {
-                    int temp = nums[i];
-                    nums[i] = nums[i+1];
-                    nums[i+1] = temp;
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
                 }
             }
         }
@@ -29,6 +29,5 @@ public class BOJ2750_2 {
         for (int num : nums) {
             System.out.println(num);
         }
-
     }
 }
