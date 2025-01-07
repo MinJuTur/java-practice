a451561ad6de6d3f6b9afe9e8e78ec284a744ab0
MinJuTur <jmj4701jmj@gmail.com>
2025-01-07 12:06:13 +0900

    Bronze_2 Day3

---
+++ src/Bronze_2/Day3/BOJ2577.java
@@ -0,0 +1,26 @@
+package Bronze_2.Day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2577 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(bf.readLine());
+        int B = Integer.parseInt(bf.readLine());
+        int C = Integer.parseInt(bf.readLine());
+        char[] result = String.valueOf(A * B * C).toCharArray();
+
+        for (int i = 0; i <= 9; i++) {
+            int count = 0;
+            for (int j = 0; j < result.length; j++) {
+                if (Integer.parseInt(String.valueOf(result[j])) == i) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
