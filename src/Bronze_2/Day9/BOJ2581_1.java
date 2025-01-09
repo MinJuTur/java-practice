84d2efe3f0d25ac3d582823e6303040248203492
MinJuTur <jmj4701jmj@gmail.com>
2025-01-09 10:41:37 +0900

    Bronze_2 Day9

---
+++ src/Bronze_2/Day9/BOJ2581_1.java
@@ -0,0 +1,37 @@
+package Bronze_2.Day9;
+
+import java.util.Scanner;
+
+public class BOJ2581_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
+        int min = 0;
+
+        if (M == 1) M++;
+        for (int i = N; i >= M; i--) { //i: N부터 M까지 -1만큼씩 줄어든다.
+            boolean isSosu = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) { //i가 소수인지 판별한다.
+                if (i % j == 0) {
+                    isSosu = false;
+                    break;
+                }
+            }
+
+            if (isSosu){
+                sum += i;
+                min = i;
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
---
+++ src/Bronze_2/Day9/BOJ2581_2.java
@@ -0,0 +1,41 @@
+package Bronze_2.Day9;
+
+import java.util.Scanner;
+
+public class BOJ2581_2 {
+    public static void main(String[] args) {
+        int[] A = new int[10001];
+        for (int i = 2; i < 10001; i++) {
+            A[i] = i;
+        }
+
+        for (int i = 2; i < Math.sqrt(10001); i++) { //제곱근까지만 돌아도 소수는 모두 판정난다.
+            if (A[i] == i) { //소수 일 때
+                for (int j = i+i; j < 10001; j+=i) { //소수의 배수는 소수가 아니므로 0으로 바꿈
+                    A[j] = 0;
+                }
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
+        int min = 0;
+
+        for (int i = N; i >= M ; i--) {
+            if (A[i] != 0) {
+                sum += A[i];
+                min = A[i];
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
