003c791ee6b2be98cc83bb3cc57df12cb15994b1
MinJuTur <jmj4701jmj@gmail.com>
2025-01-10 16:22:25 +0900

    Bronze_2 Day14-done

--- src/Bronze_2/Day14/BOJ19532.java
+++ src/Bronze_2/Day14/BOJ19532.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class BOJ19532 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double[] arr1 = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
-        double[] arr2 = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
 
-        double m1 = arr1[0];
-        double m2 = arr2[0];
-        for (int i = 0; i < 3; i++) { //x의 계수를 1로 만들기
-            arr1[i] /= m1;
-            arr2[i] /= m2;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        for (int i = -999; i <= 999; i++) {
+            for (int j = -999; j <= 999; j++) {
+                if (a*i + b*j == c && d*i + e*j == f){
+                    System.out.println(i + " " + j);
+                    break;
+                }
+            }
+
         }
-
-        double[] temp = {0, arr1[1] - arr2[1], arr1[2] - arr2[2]};
-
-        int y = (int) (temp[2] / temp[1]);
-        int x = (int) (arr1[2] - arr1[1] * y);
-
-        System.out.println(x + " " + y);
     }
 }
