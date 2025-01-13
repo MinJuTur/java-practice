1f83b27b1762ef37100d55fce2456533ce0e8ad1
MinJuTur <jmj4701jmj@gmail.com>
2025-01-13 11:24:31 +0900

    Bronze_2 Day15

---
+++ src/Bronze_2/Day15/BOJ15829.java
@@ -0,0 +1,29 @@
+package Bronze_2.Day15;
+
+import java.util.Scanner;
+
+public class BOJ15829 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int L = Integer.parseInt(sc.next());
+        char[] temps = sc.next().toCharArray();
+        long sum = 0;
+        long M = 1234567891;
+        int r = 31;
+
+        for (int i = 0; i < L; i++) {
+            long temp = temps[i] - 'a' + 1;
+            long a = 1;
+            //r의 거듭제곱을 M으로 나눈 나머지로 구해야 long 범위를 넘어가지 않음.
+            for (int j = 0; j < i; j++) {
+                a = (a * r) % M;
+            }
+
+            //M으로 나눈 나머지로 더해야 long 범위를 넘어가지 않음.
+            sum += (temp * a) % M;
+            sum %= M;
+        }
+        System.out.println(sum % M);
+    }
+}
