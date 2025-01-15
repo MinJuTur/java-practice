28646aade3295edf9c10c698d86dfaa241c14c1e
MinJuTur <jmj4701jmj@gmail.com>
2025-01-15 13:59:05 +0900

    Q007-done

--- src/AlgorithmCodingTest_JAVA/Chapter1_DataStructure/Q007.java
+++ src/AlgorithmCodingTest_JAVA/Chapter1_DataStructure/Q007.java
@@ -1,41 +1,36 @@
 package AlgorithmCodingTest_JAVA.Chapter1_DataStructure;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q007 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        long N = sc.nextLong();
-        long M = sc.nextLong();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            int temp = sc.nextInt();
-            if (temp < M) arr.add(temp);
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = sc.nextInt();
         }
-        Collections.sort(arr);
+        Arrays.sort(arr);
 
-        int start_index = 0;
-        int end_index = arr.size() - 1;
-        int sum = arr.get(start_index) + arr.get(end_index);
+        int i = 1;
+        int j = N;
         int count = 0;
 
-        while (start_index < end_index) {
-            if (sum < M) {
-                start_index++;
-            } else if (sum > M) {
-                start_index++;
-                end_index = start_index + 1;
-            } else if (sum == M) {
-                count++;
-                start_index++;
-                end_index = start_index + 1;
-            }
-            sum = arr.get(start_index) + arr.get(end_index);
-        }
+        while (i < j) {
+            int sum = arr[i] + arr[j];
 
+            if (sum < M) i++;
+            else if (sum > M) j--;
+            else if (sum == M) {
+                count++;
+                i++;
+                j--;
+            }
+        }
+        System.out.println(count);
     }
 }
