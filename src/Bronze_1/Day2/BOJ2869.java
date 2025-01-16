0cde9e2fec2a56efc57b5cbc65dcc64f6f80e439
MinJuTur <jmj4701jmj@gmail.com>
2025-01-16 14:25:37 +0900

    Bronze_1 Day2

---
+++ src/Bronze_1/Day2/BOJ2869.java
@@ -0,0 +1,19 @@
+package Bronze_1.Day2;
+
+import java.util.Scanner;
+
+public class BOJ2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt(); //낮 +A
+        int B = sc.nextInt(); //밤 -B
+        int V = sc.nextInt(); //막대기 길이
+
+        //
+        int day = (V-B) / (A-B);
+        if (((V-B) % (A-B)) != 0) day++;
+
+        System.out.println(day);
+    }
+}
---
+++ src/Bronze_1/Day2/BOJ4344_1.java
@@ -0,0 +1,33 @@
+package Bronze_1.Day2;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class BOJ4344_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int C = sc.nextInt();
+
+        for (int i = 0; i < C; i++) {
+            int N = sc.nextInt();
+
+            int[] arr = new int[N]; //N명 학생의 점수
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+            }
+
+            double average  = (double) sum / N; //평균
+            int averageOverCount = 0;
+            for (int j = 0; j < N; j++) {
+                if (arr[j] > average) averageOverCount++;
+            }
+            double portion = (double) averageOverCount / N * 100;
+            BigDecimal rounded = new BigDecimal(portion).setScale(3, RoundingMode.HALF_UP);
+            System.out.println(rounded + "%");
+        }
+    }
+}
