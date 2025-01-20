617218a606164db2fb71b86ccdbf16725d91e6e4
MinJuTur <jmj4701jmj@gmail.com>
2025-01-20 11:57:43 +0900

    Bronze1 Day6-revise

--- src/Bronze_1/Day6/BOJ10798.java
+++ src/Bronze_1/Day6/BOJ10798.java
@@ -7,13 +7,11 @@ public class BOJ10798 {
         Scanner sc = new Scanner(System.in);
 
         String[] A = new String[5];
-        int maxLength = 0;
         for (int i = 0; i < 5; i++) {
             A[i] = sc.nextLine();
-            if (A[i].length() > maxLength) maxLength = A[i].length();
         }
 
-        for (int i = 0; i < maxLength; i++) {
+        for (int i = 0; i <= 15; i++) {
             for (String s : A) {
                 if (s.length() - 1 >= i) System.out.print(s.charAt(i));
             }
