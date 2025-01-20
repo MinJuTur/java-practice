b82ca29214b4960e3ab4baac79d1deebb10d153c
MinJuTur <jmj4701jmj@gmail.com>
2025-01-20 10:18:20 +0900

    Algorithm Q011-done

--- src/AlgorithmCodingTest_JAVA/Chapter1_DataStructure/Q011.java
+++ src/AlgorithmCodingTest_JAVA/Chapter1_DataStructure/Q011.java
@@ -3,26 +3,38 @@ package AlgorithmCodingTest_JAVA.Chapter1_DataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Q011 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bf.readLine());
+        ArrayList<String> answer = new ArrayList<>();
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        int num = 1; //오름차순 자연수 수열의 역할을 한다. 항상 +1씩만 할 수 있다.
+        boolean isPossible = true;
         for (int i = 0; i < n; i++) {
-            int num = 1;
-            int temp = Integer.parseInt(bf.readLine());
-
-            while (num <= temp) {
-                arr.add(num);
-                num++;
+            int current = Integer.parseInt(bf.readLine());
+            while (current >= num) {
+                stack.push(num++);
+                answer.add("+");
             }
+            int pop = stack.pop();
+            if (pop != current) {
+                isPossible = false;
+                break;
+            } else answer.add("-");
+        }
 
-
+        if (isPossible) {
+            for (String s : answer) {
+                System.out.println(s);
+            }
+        } else {
+            System.out.println("NO");
         }
     }
 }
