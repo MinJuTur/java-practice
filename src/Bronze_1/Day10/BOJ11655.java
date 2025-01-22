f919a8b46bcb0e92aec3a27b1c65491bbbdb7b63
MinJuTur <jmj4701jmj@gmail.com>
2025-01-22 16:15:51 +0900

    Bronze1 Day10

---
+++ src/Bronze_1/Day10/BOJ11655.java
@@ -0,0 +1,19 @@
+package Bronze_1.Day10;
+
+import java.util.Scanner;
+
+public class BOJ11655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] S = sc.nextLine().toCharArray();
+        for (int i = 0; i < S.length; i++) {
+            char temp = S[i];
+            if (temp >= 65) { //알파벳일 때
+                if ((temp > 109 && temp <= 122) || (temp > 77 && temp <= 90)) { //+13을 하면 넘어가는 알파벳일 때
+                    temp -= 13;
+                } else temp += 13;
+            }
+            System.out.print(temp);
+        }
+    }
+}
---
+++ src/Bronze_1/Day10/BOJ1373.java
@@ -0,0 +1,28 @@
+package Bronze_1.Day10;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ1373 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] arr = sc.nextLine().toCharArray(); //2진수
+        ArrayList<Integer> answer = new ArrayList<>(); //8진수
+
+        for (int i = arr.length - 1; i >= 0;) {
+            //2진수 자릿수 3개 = 8진수 자릿수 1개
+            int sum = 0;
+            for (int j = 0; j < 3 &&  i >= 0; j++) {
+                int temp = Integer.parseInt(String.valueOf(arr[i--]));
+                int pow = (int) Math.pow(2, j);
+                sum += temp * pow;
+            }
+            answer.add(sum);
+        }
+
+        for (int i = answer.size() - 1; i >= 0; i--) {
+            System.out.print(answer.get(i));
+        }
+    }
+}
