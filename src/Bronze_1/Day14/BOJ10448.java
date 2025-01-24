6bf1c55bce48424c7fa843a986227ff8681d0b6f
MinJuTur <jmj4701jmj@gmail.com>
2025-01-24 16:21:59 +0900

    Bronze1 Day14-undone

---
+++ src/Bronze_1/Day14/BOJ10448.java
@@ -0,0 +1,37 @@
+package Bronze_1.Day14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10448 {
+    public static void main(String[] args) throws IOException {
+        int[] arr = new int[45]; //T45 = 990
+        arr[1] = 1;
+        for(int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i-1] + i;
+        }
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int temp = Integer.parseInt(bf.readLine());
+            boolean isOK = false;
+            while (!isOK) {
+                for (int j = 1; j < 45; j++) {
+                    for (int k = 1; k < 45; k++) {
+                        for (int l = 1; l < 45; l++) {
+                            if ((arr[j] + arr[k] + arr[l]) == temp) {
+                                isOK = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (isOK) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+}
---
+++ src/Bronze_1/Day14/BOJ1145.java
@@ -0,0 +1,7 @@
+package Bronze_1.Day14;
+
+public class BOJ1145 {
+    public static void main(String[] args) {
+
+    }
+}
