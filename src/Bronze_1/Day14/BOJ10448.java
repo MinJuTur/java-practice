f02d17b62de93711e8c77bbff100000b37a0d39f
MinJuTur <jmj4701jmj@gmail.com>
2025-02-03 22:06:47 +0900

    Bronze1 Day14-done

--- src/Bronze_1/Day14/BOJ10448.java
+++ src/Bronze_1/Day14/BOJ10448.java
@@ -6,10 +6,9 @@ import java.io.InputStreamReader;
 
 public class BOJ10448 {
     public static void main(String[] args) throws IOException {
-        int[] arr = new int[45]; //T45 = 990
-        arr[1] = 1;
-        for(int i = 2; i < arr.length; i++) {
-            arr[i] = arr[i-1] + i;
+        int[] arr = new int[45]; //i=44일 때 990이므로
+        for(int i = 1; i < arr.length; i++) {
+            arr[i] = i*(i+1)/2;
         }
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -18,17 +17,17 @@ public class BOJ10448 {
         for (int i = 0; i < T; i++) {
             int temp = Integer.parseInt(bf.readLine());
             boolean isOK = false;
-            while (!isOK) {
-                for (int j = 1; j < 45; j++) {
-                    for (int k = 1; k < 45; k++) {
-                        for (int l = 1; l < 45; l++) {
-                            if ((arr[j] + arr[k] + arr[l]) == temp) {
-                                isOK = true;
-                                break;
-                            }
+            for (int j = 1; j < 45; j++) {
+                for (int k = 1; k < 45; k++) {
+                    for (int l = 1; l < 45; l++) {
+                        if ((arr[j] + arr[k] + arr[l]) == temp) {
+                            isOK = true;
+                            break;
                         }
                     }
+                    if (isOK) break;
                 }
+                if (isOK) break;
             }
             if (isOK) System.out.println(1);
             else System.out.println(0);
