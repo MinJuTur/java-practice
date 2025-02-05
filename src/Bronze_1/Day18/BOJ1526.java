b549896f41e23622d451a64152a25967ae4b82a7
MinJuTur <jmj4701jmj@gmail.com>
2025-02-05 15:24:07 +0900

    Bronze1 Day18

---
+++ src/Bronze_1/Day18/BOJ1526.java
@@ -0,0 +1,28 @@
+package Bronze_1.Day18;
+
+import java.util.Scanner;
+
+public class BOJ1526 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = N; i >= 4 ; i--) {
+            char[] temp = String.valueOf(i).toCharArray();
+
+            boolean isOK = true;
+            for (int j = 0; j < temp.length; j++) {
+                if (temp[j] != '4' && temp[j] != '7') {
+                    isOK = false;
+                    break;
+                }
+
+            }
+            if (isOK) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
