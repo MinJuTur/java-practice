89835b605cbc8f32308e8675e6bfd61d0892fda3
MinJuTur <jmj4701jmj@gmail.com>
2025-02-06 10:54:36 +0900

    Bronze1 Day19

---
+++ src/Bronze_1/Day19/BOJ14659.java
@@ -0,0 +1,26 @@
+package Bronze_1.Day19;
+
+import java.util.Scanner;
+
+public class BOJ14659 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int maxCnt = 0;
+        int count = 0;
+        int start = sc.nextInt();
+        for (int i = 0; i < N-1; i++) {
+            int enemy = sc.nextInt();
+            if (start > enemy){
+                count++;
+            } else if (start < enemy) {
+                start = enemy;
+                count = 0;
+            }
+            if (count > maxCnt) maxCnt = count;
+        }
+        System.out.println(maxCnt);
+    }
+}
