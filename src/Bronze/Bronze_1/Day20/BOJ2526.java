3e3b669d5b2c75fd7560bfe0a053997eaad0a7e0
MinJuTur <jmj4701jmj@gmail.com>
2025-02-06 12:24:28 +0900

    Bronze1 Day20-revise

--- src/Bronze/Bronze_1/Day20/BOJ2526.java
+++ src/Bronze/Bronze_1/Day20/BOJ2526.java
@@ -15,23 +15,13 @@ public class BOJ2526 {
         int temp = N;
         while (true) {
             temp = (temp * N) % P;
-            int isHaveIndex = isHave(A, temp);
-            if (isHaveIndex != -1){ //반복구간을 찾았을 때
-                System.out.println(A.size() - isHaveIndex);
-                break;
-            } else A.add(temp);
-        }
-    }
-
-    private static int isHave(ArrayList<Integer> arr, int temp) {
-        int isHaveIndex = -1; //반복이 시작되는 지점의 인덱스
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) == temp) {
-                isHaveIndex = i;
-                break;
+            for (int i = 0; i < A.size(); i++) {
+                if (A.get(i) == temp) {
+                    System.out.println(A.size() - i);
+                    return;
+                }
             }
+            A.add(temp);
         }
-
-        return isHaveIndex;
     }
 }
