96436f299526be87402e47eb8f40b760a70130c2
MinJuTur <jmj4701jmj@gmail.com>
2025-02-11 09:21:31 +0900

    Silver5 Day4-revise

--- src/Silver/Silver_5/Day4/BOJ10814_2.java
+++ src/Silver/Silver_5/Day4/BOJ10814_2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
@@ -19,13 +20,20 @@ public class BOJ10814_2 {
             people.add(new People(Integer.parseInt(st.nextToken()), st.nextToken()));
         }
 
+        Collections.sort(people, new Comparator<People>() {
+            @Override
+            public int compare(People o1, People o2) {
+                return o1.age - o2.age;
+            }
+        });
+
         for (People person : people) {
             System.out.println(person.age + " " + person.name);
         }
     }
 }
 
-class People implements Comparator<People> {
+class People {
 
     int age;
     String name;
@@ -33,10 +41,5 @@ class People implements Comparator<People> {
     public People(int age, String name) {
         this.age = age;
         this.name = name;
-    }
-
-    @Override
-    public int compare(People o1, People o2) {
-        return 0;
     }
 }
