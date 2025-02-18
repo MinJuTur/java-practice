de4b800e7f66a3f9a4877bfaf6e97db33961f8a5
MinJuTur <jmj4701jmj@gmail.com>
2025-02-18 16:18:11 +0900

    Silver5 Day11

--- src/Silver/Silver_5/Day11/BOJ1094.java
+++ src/Silver/Silver_5/Day11/BOJ1094.java
@@ -1,26 +1,19 @@
 package Silver.Silver_5.Day11;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class BOJ1094 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
+        int count = 0;
 
-        int count = 1;
-        int sum = 64;
-        int min = 64;
-
-        while (sum > X) { //합이 X보다 크다면, 아래와 같은 과정을 반복한다
-            int half = min / 2; //가지고 있는 막대 중 길이가 가장 짧은 것을 절반으로 자른다.
-            min = half;
-
-            if (sum - half >= X) { //위에서 자른 막대의 절반 중 하나를 버리고 남아있는 막대의 길이의 합이 X보다 크거나 같다면
-                sum -= half; //위에서 자른 막대의 절반 중 하나를 버린다
-                count = count / 2;
+        //X를 2진수로 표현해서 1이 되었을 때 count 한다
+        while (X > 0) {
+            if (X % 2 == 1) {
+                count++;
             }
+            X /= 2;
         }
         System.out.println(count);
     }
